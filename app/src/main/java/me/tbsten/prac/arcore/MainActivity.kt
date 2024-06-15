622db164b2f6edf8233cf4e68268715a7dbba1b7
TBSten <81161390+TBSten@users.noreply.github.com>
2024-06-16 04:45:39 +0900

    3Dモデルを表示する画面を実装

--- app/src/main/java/me/tbsten/prac/arcore/MainActivity.kt
+++ app/src/main/java/me/tbsten/prac/arcore/MainActivity.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import io.github.sceneview.Scene
+import io.github.sceneview.math.Position
+import io.github.sceneview.node.ModelNode
+import io.github.sceneview.rememberCameraNode
+import io.github.sceneview.rememberEngine
+import io.github.sceneview.rememberModelLoader
+import io.github.sceneview.rememberNode
 import me.tbsten.prac.arcore.ui.theme.PracArCoreTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +21,53 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PracArCoreTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Bear3D("fatBear.glb")
             }
         }
     }
 }
 
+/**
+ * ref: https://github.com/SceneView/sceneview-android?tab=readme-ov-file#3d-scene-filament
+ */
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Bear3D(
+    assetFileLocation: String,
+    modifier: Modifier = Modifier,
+) {
+    val engine = rememberEngine()
+    val modelLoader = rememberModelLoader(engine)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PracArCoreTheme {
-        Greeting("Android")
+    val cameraNode = rememberCameraNode(engine).apply {
+        position = Position(z = -4f)
     }
+    val centerNode = rememberNode(engine)
+        .addChildNode(cameraNode)
+
+    Scene(
+        modifier = modifier,
+        engine = engine,
+        modelLoader = modelLoader,
+        cameraNode = cameraNode,
+        childNodes = listOf(
+            centerNode,
+            /*
+            Cannot inline bytecode built with JVM target 17 into bytecode that is being built with JVM target 1.8. Please specify proper '-jvm-target' option
+            というエラーが表示されるかもしれないが、IDEのバグ？っぽく ビルドは通るので無視して良さそう。
+            */
+            rememberNode {
+                ModelNode(
+                    modelInstance = modelLoader.createModelInstance(
+                        assetFileLocation = assetFileLocation,
+                    ),
+                    scaleToUnits = 1.0f
+                )
+            },
+        ),
+        // 背景が透明になる
+        isOpaque = false,
+        onFrame = {
+            cameraNode.lookAt(centerNode)
+        },
+    )
 }
